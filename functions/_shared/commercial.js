8a5c62e9a9f3f0f1e48d028b8963dcefbb6d33cf
refreshlaser <hello@refreshlaser.com.au>
2026-08-12 10:14:40 +1000

    Update SiteRefresh pricing tiers

--- functions/_shared/commercial.js
+++ functions/_shared/commercial.js
@@ -1,4 +1,4 @@
-export const STANDARD_PROJECT_EX_GST_CENTS = 395_000;
+export const STANDARD_PROJECT_EX_GST_CENTS = 195_000;
 export const GST_RATE_PERCENT = 10;
 export const DEPOSIT_PERCENT = 30;
 export const HOSTING_EX_GST_CENTS = 8_900;
